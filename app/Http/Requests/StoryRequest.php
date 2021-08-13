d57456deb0aa7d1ec5f6576605b4eaaf981f9d5c
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-08-13 16:21:26 +0630

    Request updated

--- app/Http/Requests/StoryRequest.php
+++ app/Http/Requests/StoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoryRequest extends FormRequest
 {
@@ -24,10 +25,32 @@ class StoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required',
+            'title' => [
+                'required', 'min:10', 'max:40',
+                function ($attributes, $value, $fail) {
+                    if ($value == 'Dummy Title') {
+                        $fail($attributes . ' is not valid.');
+                    }
+                },
+                Rule::unique('stories'),
+            ],
+            'body' => ['required', 'min:50'],
             'type' => 'required',
-            'body' => 'required',
             'status' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            // 'required' => 'fuck you! enter or select this :attribute',
+            'title.required' =>
+            'You must enter :attribute.',
+            'body.required' =>
+            'You must enter :attribute.',
+            'type.required' =>
+            'You must select :attribute.',
+            'status.required' => 'You must choose :attribute.',
         ];
     }
 }
